a540d9afc7afdc1235b64adce50f4ab50648f79d
Fatih3457 <fatihozcan90@outlook.com>
2023-07-30 19:26:36 +0300

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import "./home.css"
+
+export const Home = () => {
+  return (
+    <div className="container">
+        <div className="home-page"></div>
+        <div className="home-page-left"></div>
+        <div className="home-page-right"></div>
+    </div>
+  );
+};
