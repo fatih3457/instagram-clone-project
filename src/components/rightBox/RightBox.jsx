6807f231888bd8a13318254f79cde9f8c9692f24
Fatih3457 <fatihozcan90@outlook.com>
2023-08-06 18:35:53 +0300

    sixth commit

--- src/components/rightBox/RightBox.jsx
+++ src/components/rightBox/RightBox.jsx
@@ -1,7 +1,48 @@
+import { Avatar} from "@mui/material";
+import { Button } from "@mui/material";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+
 import "./rightBox.css";
 
 export const RightBox = () => {
   return (
-    <div>RightBox</div>
+    <div className="right-box">
+      <div className="account">
+        <a href="/">
+          <Avatar
+            src="/images/3chicken.jpg"
+            sx={{ width: 56, height: 56 }} />
+        </a>
+        <a href="/" className="account-titles">
+          <a href="/">fatihozcan</a>
+          <span>Fatih Özcan</span>
+        </a>
+      </div>
+      <span className="friends-title">My Friends</span>
+      <ul className="friends-list">
+        <li className="friend-item">
+          <div className="friend-item-left">
+          <a href="/">
+          <Avatar
+            src="/images/2blackbean.jpg"
+            sx={{ width: 56, height: 56 }} />
+        </a>
+        <div className="friend-username">
+          <a href="/">rasit</a>
+          <span>Raşit Özcan</span>
+        </div>
+          </div>
+          <div className="friend-item-right">
+            <Button
+              variant="contained"
+              endIcon= {<ArrowCircleRightOutlinedIcon />}
+              size="small"
+            >
+              Profile Git
+            </Button>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
