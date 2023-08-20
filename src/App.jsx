dd2bf695b4b6f0fbf4a3a7a641b60dd718fff7b0
Fatih3457 <fatihozcan90@outlook.com>
2023-08-20 19:41:34 +0300

    fifteenth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { Header } from "./components/header/Header";
 import { Share } from "./components/share/Share";
-/* import { Register } from "./pages/home/register/Register"; */
+import { Register } from "./pages/home/register/Register";
 import { Login } from "./pages/home/login/Login";
-/* import { Messenger } from "./pages/home/messenger/Messenger"; */
-/* import { Home } from "./pages/home/Home"; */
-/* import { Profile } from "./pages/home/profile/Profile"; */
+import { Messenger } from "./pages/home/messenger/Messenger";
+import { Home } from "./pages/home/Home";
+import { Profile } from "./pages/home/profile/Profile";
 
 function App() {
   const [open, setOpen] = useState(false); 
@@ -15,10 +15,10 @@ function App() {
     <div className="App">
       <Header handleOpen={handleOpen} /> 
       <Share open={open} handleClose={handleClose} />
-      {/* <Home /> */}
-      {/* <Profile /> */}
-      {/* <Messenger /> */}
-      {/* <Register /> */}
+      <Home />
+      <Profile />
+      <Messenger />
+      <Register />
       <Login />
     </div>
   );
